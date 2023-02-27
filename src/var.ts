9fba70660c8056963c93197766a1cbf3d4c929ea
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-02-27 22:27:51 +0530

    new variable declared

--- src/var.ts
+++ src/var.ts
@@ -1,2 +1,4 @@
 export const animatedImage:HTMLElement | null = document.getElementById('animated-image')
-export const todoList: HTMLElement | null = document.getElementById('todo-list')
+export const todoList = (document.getElementById('todo-list') as HTMLElement)
+export const addBtn: HTMLElement | null = document.getElementById('add-btn')
+export const inputTask= (document.getElementById('input-Task') as HTMLInputElement)
