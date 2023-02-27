c4105669cb524bd491001133a7bfa040903357d9
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-02-27 22:26:45 +0530

    added Functions
    added update DOM functions
    added Database update function on input from user

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
-import './style.css'
-import "@lottiefiles/lottie-player";
-import { animatedImage, todoList } from "./var";
-import {getAllTodos }from '../api/db-functions/getallTodos'
+import './style.css';
+import '@lottiefiles/lottie-player';
+import { addBtn, animatedImage, inputTask, todoList } from './var';
+import getTodos from '../api/db-functions/getallTodos';
+import { insertTodo } from '../api/db-functions/insertTodo';
+let text: string = 'hello';
 
-// if (getAllTodos != null) {
-  // todoList.innerHTML = `${getAllTodos}`
-// } else {
-  // console.log('nothing')
-// }
+const updateTodoList = () => {
+  todoList.innerHTML = '';
+  getTodos().then((getAllTodos) => {
+    getAllTodos.forEach((todo) => {
+      const div = document.createElement('div');
+      todoList?.insertAdjacentHTML(
+        'afterbegin',
+        `      <div class="w-[60%] bg-Sunglow p-2 rounded-xl text-black m-1 shadow-lg shadow-black-500/50" id="xyz">
+    <input type="checkbox" id="${todo.id}"/> ${todo.Task}
+  </div>`
+      );
+    });
+  });
+};
+
+if (document.readyState === 'complete') {
+  updateTodoList();
+}
+
+addBtn?.addEventListener('click', () => {
+  text = inputTask?.value;
+  insertTodo(text);
+  updateTodoList();
+});
