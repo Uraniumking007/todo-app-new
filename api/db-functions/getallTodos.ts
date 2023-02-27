55ab189a4bdd3344dfd728c97ea63b6b5213eb01
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-02-27 22:26:57 +0530

    fetch db data

--- api/db-functions/getallTodos.ts
+++ api/db-functions/getallTodos.ts
@@ -1,6 +1,12 @@
 // Generated with CLI
 import { getXataClient } from '../xata';
-
+import { addBtn } from '../../src/var';
 const xata = getXataClient();
+// let getAllTodos
 
-export const getAllTodos = await xata.db["todo-list"].getAll();
+async function getTodos() {
+    let getAllTodos = await xata.db["todo-list"].getAll();
+    return getAllTodos
+}
+
+export default getTodos;
