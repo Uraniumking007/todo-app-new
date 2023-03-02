3ff37f541439a8c822f4dd15669ac5109e5c846d
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-02 23:18:48 +0530

    changed client initialization location

--- functions/getTodos/getTodos.ts
+++ functions/getTodos/getTodos.ts
@@ -1,9 +1,8 @@
 import { Handler } from '@netlify/functions';
 import { getXataClient } from '../../api/xata';
 export const handler: Handler = async (event, context) => {
-  // const { name = 'strangr' } = event.queryStringParameters;
-  const xata = getXataClient();
   try {
+    const xata = getXataClient();
     let allTodos = await xata.db['todo-list'].getAll();
     return {
       statusCode: 200,
