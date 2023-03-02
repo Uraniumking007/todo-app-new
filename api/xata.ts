5e31834dcc8755f166e2f697ddca5a91cbe94024
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-02 19:29:06 +0530

    made changes in xata

--- api/xata.ts
+++ api/xata.ts
@@ -30,8 +30,7 @@ const DatabaseClient = buildClient();
 
 const defaultOptions = {
   databaseURL: 'https://Todo-App-5gqroj.us-east-1.xata.sh/db/todo-app',
-  enableBrowser: true,
-  apiKey: 'xau_mTVXCHBPilru8a0SUie6oMbuxsYBzklM1',
+  apiKey: process.env.Xata_Api_Secret,
 };
 
 export class XataClient extends DatabaseClient<DatabaseSchema> {
