75ad037e0d947db0c2c9b61bb5c274a9684f99b3
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-02 19:34:25 +0530

    added insertTodos

---
+++ utils/insertTodos.ts
@@ -0,0 +1,16 @@
+export const insertTodo = async (task: string) => {
+  const data = {
+    Task: `${task}`,
+    isDone: false,
+  };
+  try {
+    const todos = await fetch('/.netlify/functions/insertItem', {
+      method: 'POST',
+      headers: { contentType: 'application/json' },
+      body: JSON.stringify(data),
+    });
+    return console.log('Successfully inserted');
+  } catch (error) {
+    console.log(error);
+  }
+};
