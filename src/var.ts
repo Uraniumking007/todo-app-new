ad3c59703c956a8664163d37ab3ebcdfe0e1ca2b
UraniumKing <96092183+Uraniumking007@users.noreply.github.com>
2023-03-04 14:38:25 +0530

    added new element

--- src/var.ts
+++ src/var.ts
@@ -1,6 +1,9 @@
 export const animatedImage: HTMLElement | null =
   document.getElementById('animated-image');
 export const todoList = document.getElementById('todo-list') as HTMLElement;
+export const todoListTrue = document.getElementById(
+  'todo-list-true'
+) as HTMLElement;
 export const addBtn: HTMLElement | null = document.getElementById('add-btn');
 export const inputTask = document.getElementById(
   'input-Task'
